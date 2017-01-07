b79da48f7cef277ca8117614750ce573b1cfbded
Pearl Li <pearlzli16@gmail.com>
2017-01-07 00:51:19 -0500

    Block out hospitals.jla nd allocations.jl

--- kidney_exchange/kidney_exchange.jl
+++ kidney_exchange/kidney_exchange.jl
@@ -12,11 +12,17 @@ module KidneyExchange
         Person, Patient, Donor, are_compatible,
         Pair, IncompatiblePair, CompatiblePair,
 
+        # hospitals.jl
+
         # compatibility_graph.jl
         CompatibilityGraph, get_donors, get_patients, get_pair
 
+        # allocations.jl
+
     include("blood_type.jl")
     include("pairs.jl")
+    include("hospitals.jl")
     include("compatibility_graph.jl")
+    include("allocations.jl")
 
 end
